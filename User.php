6584875d117b943dc87fc580f703af2b584f79e6
StefanoMast <ste.mastrantonio@gmail.com>
2022-06-14 16:49:26 +0200

    add user, methods and constructor

--- User.php
+++ User.php
@@ -4,20 +4,36 @@ class Utente {
 
     public $name;
     public $email;
+    public $isRegistered = false;
+    public $creditCard= 2022
     // All'interno del carrello ci sono dei veicoli
-    public $cart = [];
+    public $products = [];
 
     function __construct($_name, $_email) {
         $this->name = $_name;
         $this->email = $_email;
     }
 
+    public function register()
+    {
+        $this->isRegistered = true;
+    }
+
+    public function valid()
+    {
+        if ($this->valid >= 2022) {
+            $this->products = "ok, puoi effettuare l'acquisto";
+        } else {
+            $this->products = "carta non valida";
+        }
+    }
+
     // aggiunge un prodotto al carello, se disponibile
     // $_product -> un prodotto di tipo Veicolo
     // return true se aggiunge, false altrimenti
-    function addProductToCart($_product) {
-        if ($_product->disponibile) {
-            $this->cart[] = $_product;
+    function addProductToProducts($_product) {
+        if ($_product->available) {
+            $this->products[] = $_product;
             return true;
         } else {
             return false;
@@ -26,9 +42,23 @@ class Utente {
 
     function getTotalPrice() {
         $total_price = 0;
-        foreach($this->cart as $item) {
-            $total_price += $item->prezzo;
+        foreach($this->products as $item) {
+            $total_price += $item->price;
         }
-        return $total_price;
+
+        if ($this->isRegistered == true) {
+            $total_price = $total_price - ($total_price * 20 / 100);
+        }else {
+            return $total_price;
+        }
+
+        function getTotalPrice() {
+            $total_price = 0;
+            foreach($this->products as $item) {
+                $total_price += $item->price;
+            }
+            return $total_price;
+        }
+            
     }
 }
